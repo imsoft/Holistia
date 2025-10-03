db53dc4f339e190dba1f616f8f8d730592ce5e81
Brandon Uriel Garcia Ramos <weareimsoft@gmail.com>
2025-10-03 10:10:06 -0600

    Actualizar página de exploración
    - Mejoras menores en la página de exploración de pacientes

--- src/app/(dashboard)/(patient)/patient/[id]/explore/page.tsx
+++ src/app/(dashboard)/(patient)/patient/[id]/explore/page.tsx
@@ -320,9 +320,9 @@ const HomeUserPage = () => {
           <div className="px-6 py-6 sm:py-12 lg:px-8">
             <div className="mx-auto max-w-4xl text-center">
               <h2 className="text-4xl font-semibold tracking-tight text-balance text-foreground sm:text-5xl">
-                Profesionales de Salud Mental
+                Profesionales de Salud
               </h2>
-              <p className="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-muted-foreground">
+              <p className="mx-auto mt-6 max-w-2xl text-lg/8 text-pretty text-muted-foreground">
                 Encuentra el profesional adecuado para tu bienestar mental y emocional.
               </p>
             </div>

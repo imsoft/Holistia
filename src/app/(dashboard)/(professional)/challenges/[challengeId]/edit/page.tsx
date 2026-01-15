efd6853749d7eebbcaf905be57d71e4d51d92fae
Brandon Uriel Garcia Ramos <weareimsoft@gmail.com>
2026-01-15 16:20:14 -0600

    Reordenar secciones: Recursos del Reto arriba, Reuniones Programadas abajo

--- src/app/(dashboard)/(professional)/challenges/[challengeId]/edit/page.tsx
+++ src/app/(dashboard)/(professional)/challenges/[challengeId]/edit/page.tsx
@@ -7,6 +7,7 @@ import { useUserStoreInit } from "@/hooks/use-user-store-init";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, Loader2 } from "lucide-react";
 import { ChallengeForm } from "@/components/challenges/challenge-form";
+import { ChallengeResourcesManager } from "@/components/challenges/challenge-resources-manager";
 import { ChallengeMeetingsManager } from "@/components/challenges/challenge-meetings-manager";
 import { createClient } from "@/utils/supabase/client";
 import { toast } from "sonner";
@@ -104,6 +105,8 @@ export default function EditProfessionalChallengePage() {
             redirectPath={`/challenges`}
             userType="professional"
           />
+
+          <ChallengeResourcesManager challengeId={challengeId} />
 
           <ChallengeMeetingsManager challengeId={challengeId} />
         </div>
